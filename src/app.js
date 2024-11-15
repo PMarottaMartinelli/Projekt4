e521146cccdadb7b01dfc7fe2e398b93ab223889
Paolo <paolo-marotta@protonmail.com>
2024-11-15 11:17:08 +0100

    MongoDB connection Problem

--- src/app.js
+++ src/app.js
@@ -3,19 +3,19 @@
 require('dotenv').config()
 const express = require('express');
 const path = require('path');
-const { createkursdatum,readkursdatum,toggleAnwesend, leseUser } = require('./services/mongoservice');
+const {leseUebersicht, leseItems} = require('./services/mongoservice');
 const app = express();
-const Logger = require("./models/Logger");
+//const Logger = require("./models/Logger");
 
 // /**
 //  * mit @logger .log kann ein die @Logger Klasse aufgerufen
 //  * und eine vordefinierte Anzahl von Nachrichten @msg in der Konsole angezeigt werden.
 //  * Die @Logger Klasser kann alle Konsole-Ausgaben auf einmal aktivieren oder deaktiveren, zb. den @DEBUG_MODE  
 //  */
-let logger = new Logger()
+//let logger = new Logger()
 
 // // eslint-disable-next-line no-undef
-logger.log("Debug mode mit logger",process.env.DEBUG_MODE)
+//logger.log("Debug mode mit logger",process.env.DEBUG_MODE)
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
@@ -37,8 +37,7 @@ app.get(`/favicon.ico`,(req, res) => res.status(204));
 
 app.get('/', async (req, res) => {
   try{ 
-    checkToken(req, res);
-    let kurse = await leseKurse()
+    let Items = await leseUebersicht()
     let kursarray = []
     kurse.forEach((item) => { 
       let id = item [0]
@@ -61,63 +60,63 @@ app.get('/', async (req, res) => {
 /**
  * Aufruf für die zweite Webseite.
  */
-app.get('/datum/:kursnr', async (req, res) => {
+// app.get('/datum/:kursnr', async (req, res) => {
   
-  checkToken(req, res);
-  let kursnr= req.params.kursnr;
-  logger.log(kursnr)
-  let kursrow = await leseKurs(kursnr);
-  let kurs = {Name: kursrow[1], id:kursrow[0],Wotag: Math.floor (kursrow[0]/10)}
-  let Wotag = kurs.Wotag;
-  let datums = calcDatum(Wotag);
-  logger.log(datums);
-  res.render('datum', {
-    datums: datums, kursnr: kursnr
-  });
+//   checkToken(req, res);
+//   let kursnr= req.params.kursnr;
+//   logger.log(kursnr)
+//   let kursrow = await leseKurs(kursnr);
+//   let kurs = {Name: kursrow[1], id:kursrow[0],Wotag: Math.floor (kursrow[0]/10)}
+//   let Wotag = kurs.Wotag;
+//   let datums = calcDatum(Wotag);
+//   logger.log(datums);
+//   res.render('datum', {
+//     datums: datums, kursnr: kursnr
+//   });
   
-});
+//});
 
 // Aufruf für die dritte Webseite
-app.get('/teilnehmer/:kursid', async (req, res) => {
-  checkToken(req, res);
-  let kursid= parseInt(req.params.kursid);
-  let daturl= req.query.datum;
-  let teilnmrs;
-  let kd= await readkursdatum(kursid+daturl.replaceAll(".","_"))
-  //logger.log(kd)
-  if (!kd)
-  {
-    let rows = await leseTeilnehmer(kursid);
-    //console.log ("nach leseTeilnehmer", rows)
-    let teilnehmers = []
-    rows.forEach((row) => {
+// app.get('/teilnehmer/:kursid', async (req, res) => {
+//   checkToken(req, res);
+//   let kursid= parseInt(req.params.kursid);
+//   let daturl= req.query.datum;
+//   let teilnmrs;
+//   let kd= await readkursdatum(kursid+daturl.replaceAll(".","_"))
+//   //logger.log(kd)
+//   if (!kd)
+//   {
+//     let rows = await leseTeilnehmer(kursid);
+//     //console.log ("nach leseTeilnehmer", rows)
+//     let teilnehmers = []
+//     rows.forEach((row) => {
 
-      let tn = {Vorname: row[2], Nachname: row[3], id:parseInt(row[14]), anwesend: false}
-      teilnehmers.push(tn)
+//       let tn = {Vorname: row[2], Nachname: row[3], id:parseInt(row[14]), anwesend: false}
+//       teilnehmers.push(tn)
        
-    })
-    teilnehmers.sort((row1 , row2) => {
-      if (row1.Vorname < row2.Vorname) {
-        return -1  
-      }
-      else if (row1.Vorname > row2.Vorname) {
-        return +1
-      }
-      else {
-        return 0
-      }
-    })
-    let kursdatum = {id: kursid+daturl.replaceAll(".","_"), Teilnehmer: teilnehmers}  
-    await createkursdatum(kursdatum);
-    teilnmrs = teilnehmers
-  }
-  else{
-    teilnmrs= kd.Teilnehmer;
-  }
-  res.render('teilnehmer', {
-    teilnmr: teilnmrs, kursnr: kursid, datum: daturl, 
-  });
-});
+//     })
+//     teilnehmers.sort((row1 , row2) => {
+//       if (row1.Vorname < row2.Vorname) {
+//         return -1  
+//       }
+//       else if (row1.Vorname > row2.Vorname) {
+//         return +1
+//       }
+//       else {
+//         return 0
+//       }
+//     })
+//     let kursdatum = {id: kursid+daturl.replaceAll(".","_"), Teilnehmer: teilnehmers}  
+//     await createkursdatum(kursdatum);
+//     teilnmrs = teilnehmers
+//   }
+//   else{
+//     teilnmrs= kd.Teilnehmer;
+//   }
+//   res.render('teilnehmer', {
+//     teilnmr: teilnmrs, kursnr: kursid, datum: daturl, 
+//   });
+// });
 
 // eslint-disable-next-line no-undef
 /**
@@ -127,6 +126,9 @@ const port = (process.env.PORT || 3000);
 //npm zeigt an, welcher Port ausgewählt ist
 
 app.listen(port, () => {
-    logger.log(`Server listening on Port ${port}`);
+    //logger.log(`Server listening on Port ${port}`);
 
 });
+
+console.log("Hier passeirt noch nicht viel")
+console.log("aber zumindest gibt es keinen Fehler")
--- src/services/mongoservice.js
+++ src/services/mongoservice.js
@@ -1,31 +1,23 @@
 const { MongoClient } = require('mongodb');
 
-// eslint-disable-next-line no-undef
-const url = process.env.MONGO_URL;
+const url = process.env.MONGO_URL || "mongodb://localhost:27017";
 console.log("url",url)
 const client = new MongoClient(url);
 
-const dbName = 'Aufgabe2';
+const dbName = 'ItemDB';
 
 
-/**
- * @reminder async Funktionen Nachlesen und hier eintragen
- * @returns 
- */
-
-
-
-async function readkurse() {
-    let db, kurscollection;
+async function leseUebersicht() {
+    let db, Itemcollection;
     try{
         // Use connect method to connect to the server
         await client.connect();
         console.log('Connected successfully to server');
         db = client.db(dbName);
-        kurscollection = db.collection('Kurse');
+        Itemcollection = db.collection('Kurse');
     
         // the following code examples can be pasted here...
-        const findResult = await kurscollection.find({}).toArray();
+        const findResult = await Itemcollection.find({}).toArray();
         console.log('Found documents =>', findResult);
         return findResult;
     }catch(err){
@@ -36,126 +28,126 @@ async function readkurse() {
     }
 }
 
-async function leseUser(userName) {
-    try {
-      // Use connect method to connect to the server
-      await client.connect();
-      const db = client.db(dbName);
-      const collection = db.collection('User');
+// async function leseUser(userName) {
+//     try {
+//       // Use connect method to connect to the server
+//       await client.connect();
+//       const db = client.db(dbName);
+//       const collection = db.collection('User');
   
-      const findResult = await collection.find({'user': userName}).toArray();
-      console.log(`readUser(${userName})`, 'Found documents =>', findResult);
-      if (findResult.length > 0) {
-        return findResult[0];
-      } else {
-        return null;
-      }
+//       const findResult = await collection.find({'user': userName}).toArray();
+//       console.log(`readUser(${userName})`, 'Found documents =>', findResult);
+//       if (findResult.length > 0) {
+//         return findResult[0];
+//       } else {
+//         return null;
+//       }
   
-    } catch(err) {
-      console.error(`readUser(${userName})`, err.message);
-      throw err;
-    }
-}
+//     } catch(err) {
+//       console.error(`readUser(${userName})`, err.message);
+//       throw err;
+//     }
+// }
   
 
 
 
 
 
-async function readkursdatum(id) {
-    let db, kurscollection;
-    console.log("readkursdatum",id);
-    try{
-        // Use connect method to connect to the server
-        await client.connect();
-        console.log('Connected successfully to server');
-        db = client.db(dbName);
-        kurscollection = db.collection('Kursdatum');
+// async function readkursdatum(id) {
+//     let db, kurscollection;
+//     console.log("readkursdatum",id);
+//     try{
+//         // Use connect method to connect to the server
+//         await client.connect();
+//         console.log('Connected successfully to server');
+//         db = client.db(dbName);
+//         kurscollection = db.collection('Kursdatum');
     
-        // the following code examples can be pasted here...
-        let query={"id":id};
-        const findResult = await kurscollection.find(query).toArray();
-        console.log('Found documents =>', findResult);
-        return findResult[0];
-    }catch(err){
-        console.error(err.stack)
-        throw err
-    }finally{
-        client.close()
-    }
+//         // the following code examples can be pasted here...
+//         let query={"id":id};
+//         const findResult = await kurscollection.find(query).toArray();
+//         console.log('Found documents =>', findResult);
+//         return findResult[0];
+//     }catch(err){
+//         console.error(err.stack)
+//         throw err
+//     }finally{
+//         client.close()
+//     }
 
-}
+// }
 
-async function createkursdatum(kursdatum) {
-    let db, kurscollection;
-    try{
-        // Use connect method to connect to the server
-        await client.connect();
-        console.log('Connected successfully to server');
-        db = client.db(dbName);
-        kurscollection = db.collection('Kursdatum');
+// async function createkursdatum(kursdatum) {
+//     let db, kurscollection;
+//     try{
+//         // Use connect method to connect to the server
+//         await client.connect();
+//         console.log('Connected successfully to server');
+//         db = client.db(dbName);
+//         kurscollection = db.collection('Kursdatum');
     
-        console.log("vor dem insertMany",kursdatum)
-        const findResult = await kurscollection.insertMany([ kursdatum ]);
-        console.log('insert documents(kursdatum) =>', findResult);
-        return findResult;
-    }catch(err){
-        console.error(err.stack)
-        throw err
-    }finally{
-        client.close()
-    }
-}
+//         console.log("vor dem insertMany",kursdatum)
+//         const findResult = await kurscollection.insertMany([ kursdatum ]);
+//         console.log('insert documents(kursdatum) =>', findResult);
+//         return findResult;
+//     }catch(err){
+//         console.error(err.stack)
+//         throw err
+//     }finally{
+//         client.close()
+//     }
+// }
 
-async function toggleAnwesend(kursdatumId, teilnehmerId) {
-    console.info(`toggleAnwesend(${kursdatumId},${teilnehmerId})`);
-    try{
-        await client.connect();
-        console.log('Connected successfully to server');
-        const db = client.db(dbName);
-        const kurscollection = db.collection('Kursdatum');
+// async function toggleAnwesend(kursdatumId, teilnehmerId) {
+//     console.info(`toggleAnwesend(${kursdatumId},${teilnehmerId})`);
+//     try{
+//         await client.connect();
+//         console.log('Connected successfully to server');
+//         const db = client.db(dbName);
+//         const kurscollection = db.collection('Kursdatum');
 
-        let kursdatums = await kurscollection.find({ id: kursdatumId }).toArray();
-        if (kursdatums && kursdatums.length > 0) {
-            let kursdatum = kursdatums[0];
+//         let kursdatums = await kurscollection.find({ id: kursdatumId }).toArray();
+//         if (kursdatums && kursdatums.length > 0) {
+//             let kursdatum = kursdatums[0];
             
-            let newValue = true;
-            for (let i=0; i<kursdatum.Teilnehmer.length; i++) {
-                if (kursdatum.Teilnehmer[i].id == teilnehmerId) {
-                    newValue =  !kursdatum.Teilnehmer[i].anwesend;
-                    break;
-                }
-            }
+//             let newValue = true;
+//             for (let i=0; i<kursdatum.Teilnehmer.length; i++) {
+//                 if (kursdatum.Teilnehmer[i].id == teilnehmerId) {
+//                     newValue =  !kursdatum.Teilnehmer[i].anwesend;
+//                     break;
+//                 }
+//             }
             
-            let query = { id: kursdatumId, "Teilnehmer.id": parseInt(teilnehmerId) };
-            let update = { $set: { "Teilnehmer.$.anwesend": newValue } };
-            console.info("toggleAnwesend()", kursdatumId, teilnehmerId, newValue, query, update);
-            let updateResult = await kurscollection.updateOne(
-                query,
-                update
-            );
-            console.log(`toggleAnwesend()`, 'updateResult =>', updateResult);
-            return newValue;
-        }   else {
-            // Fehler ausgeben!
-            console.log("Kursdatum nicht gefunden")
-            throw new Error(`toggleAnwesend() Kursdatum mit id=${kursdatumId} nicht gefunden!`);
-            }
-    }catch(err){
-        console.error(err.stack)
-        throw err
-    }finally{
-        client.close()
-    }
-}        
+//             let query = { id: kursdatumId, "Teilnehmer.id": parseInt(teilnehmerId) };
+//             let update = { $set: { "Teilnehmer.$.anwesend": newValue } };
+//             console.info("toggleAnwesend()", kursdatumId, teilnehmerId, newValue, query, update);
+//             let updateResult = await kurscollection.updateOne(
+//                 query,
+//                 update
+//             );
+//             console.log(`toggleAnwesend()`, 'updateResult =>', updateResult);
+//             return newValue;
+//         }   else {
+//             // Fehler ausgeben!
+//             console.log("Kursdatum nicht gefunden")
+//             throw new Error(`toggleAnwesend() Kursdatum mit id=${kursdatumId} nicht gefunden!`);
+//             }
+//     }catch(err){
+//         console.error(err.stack)
+//         throw err
+//     }finally{
+//         client.close()
+//     }
+// }        
 
 //Tabelle der User
 //Entsprechende Zugriffsfunktionen
 
 module.exports = {
-    readkurse,
-    createkursdatum,
-    readkursdatum,
-    toggleAnwesend,
-    leseUser,
+    leseUebersicht,
+    // createkursdatum,
+    // readkursdatum,
+    // toggleAnwesend,
+    // leseUser,
 }
